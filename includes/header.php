01f062ce0b77fccb645198c4a2597004a5a9e1b2
KostasEreksonas <k.ereksonas@gmail.com>
2025-09-09 21:58:09 +0300

    Like button; Profile page

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,7 @@
 <?php  
 require 'config/config.php';
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
 
 
 if (isset($_SESSION['username'])) {
@@ -20,12 +22,20 @@ else {
 	<!-- Javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/bootbox.min.js"></script>
+	<script src="assets/js/demo.js"></script>
+	<script src="assets/js/jquery.jcrop.js"></script>
+	<script src="assets/js/jcrop_bits.js"></script>
+
+
+
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
+	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
+	
 </head>
 <body>
 
@@ -48,7 +58,7 @@ else {
 			<a href="#">
 				<i class="fa fa-bell fa-lg"></i>
 			</a>
-			<a href="#">
+			<a href="requests.php">
 				<i class="fa fa-users fa-lg"></i>
 			</a>
 			<a href="#">
